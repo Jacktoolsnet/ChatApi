8441c13fac94fbe1e202a301efadcc4d2e4e26f2
Christian Wagner <jacktoolsnet@gmail.com>
2022-08-28 01:14:39 +0200

    Headers
    three headers

--- controllers/ipController.js
+++ controllers/ipController.js
@@ -1,14 +1,17 @@
 exports.getClientIp = (req, res, next) => {
     //For this solution proxy_set_header X-Real-IP $remote_addr; have to be set in the 
-    const client_ip = req.headers['x-real-ip']?.split(',').shift() || req.socket?.remoteAddress;
+    const x_real_ip = req.headers['x-real-ip']?.split(',').shift();
+    const x_forwarded_for = req.headers['x-forwarded-for']?.split(',').shift();
+    const x_forwarded_host = req.headers['x-forwarded-host']?.split(',').shift();
     res.status(200).send({
       status: {
         code: 200,
         message: 'OK'
       },
       data: {
-        client_ip,
-        x_forwarded_for: req.headers
+        x_real_ip,
+        x_forwarded_for,
+        x_forwarded_host
       }
     });
   }
