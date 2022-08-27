e55ae8c6e78920f99b7e79d9924454ba3973e7a2
Christian Wagner <jacktoolsnet@gmail.com>
2022-08-28 01:15:40 +0200

    Update ipController.js

--- controllers/ipController.js
+++ controllers/ipController.js
@@ -1,5 +1,7 @@
 exports.getClientIp = (req, res, next) => {
-    //For this solution proxy_set_header X-Real-IP $remote_addr; have to be set in the 
+    // X-Real-IP: $remote_addr;
+    // X-Forwarded-For: $proxy_add_x_forwarded_for;
+    // X-Forwarded-Host: $server_name;
     const x_real_ip = req.headers['x-real-ip']?.split(',').shift();
     const x_forwarded_for = req.headers['x-forwarded-for']?.split(',').shift();
     const x_forwarded_host = req.headers['x-forwarded-host']?.split(',').shift();
