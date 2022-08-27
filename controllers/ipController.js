605a956b394943659a0206638548bdeb5d17ef82
Christian Wagner <jacktoolsnet@gmail.com>
2022-08-28 00:03:28 +0200

    getClientIp
    with header

--- controllers/ipController.js
+++ controllers/ipController.js
@@ -1,11 +1,12 @@
 exports.getClientIp = (req, res, next) => {
+    const client_ip = req.headers['x-forwarded-for']?.split(',').shift() || req.socket?.remoteAddress;
     res.status(200).send({
       status: {
         code: 200,
         message: 'OK'
       },
       data: {
-        client_ip: req.socket.remoteAddress
+        client_ip
       }
     });
   }
