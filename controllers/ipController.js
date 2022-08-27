5a89c8a1a9ad0a484d7d3da7a56b7a0927edd6c7
Christian Wagner <jacktoolsnet@gmail.com>
2022-08-28 00:12:56 +0200

    getClientIp
    Print all headers

--- controllers/ipController.js
+++ controllers/ipController.js
@@ -8,7 +8,7 @@ exports.getClientIp = (req, res, next) => {
       },
       data: {
         client_ip,
-        x_forwarded_for: req.headers['x-real-ip']
+        x_forwarded_for: req.headers
       }
     });
   }
