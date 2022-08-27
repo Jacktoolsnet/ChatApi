0596fc725929d2962bdf54ef0aa7d69dda7cc38d
Christian Wagner <jacktoolsnet@gmail.com>
2022-08-28 01:16:49 +0200

    Update ipController.js

--- controllers/ipController.js
+++ controllers/ipController.js
@@ -2,9 +2,9 @@ exports.getClientIp = (req, res, next) => {
     // X-Real-IP: $remote_addr;
     // X-Forwarded-For: $proxy_add_x_forwarded_for;
     // X-Forwarded-Host: $server_name;
-    const x_real_ip = req.headers['x-real-ip']?.split(',').shift();
+    const x_real_ip = req.headers['x-real-ip'];
     const x_forwarded_for = req.headers['x-forwarded-for']?.split(',').shift();
-    const x_forwarded_host = req.headers['x-forwarded-host']?.split(',').shift();
+    const x_forwarded_host = req.headers['x-forwarded-host'];
     res.status(200).send({
       status: {
         code: 200,
