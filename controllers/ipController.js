5a5ffeb715a680ae20c0f80d39e13e31145c95c5
Christian Wagner <jacktoolsnet@gmail.com>
2022-08-29 20:46:51 +0200

    request-ip
    back to request ip

--- controllers/ipController.js
+++ controllers/ipController.js
@@ -1,20 +1,14 @@
+const requestIp = require('request-ip');
+
 exports.getClientIp = (req, res, next) => {
-    // X-Real-IP: $remote_addr;
-    // X-Forwarded-For: $proxy_add_x_forwarded_for;
-    // X-Forwarded-Host: $server_name;
-    const x_real_ip = req.headers['x-real-ip'];
-    const x_forwarded_for = req.headers['x-forwarded-for'];
-    const x_forwarded_host = req.headers['x-forwarded-host'];
+    const client_ip = requestIp.getClientIp(req); 
     res.status(200).send({
       status: {
         code: 200,
         message: 'OK'
       },
       data: {
-        client_ip: req.ip,
-        x_real_ip,
-        x_forwarded_for,
-        x_forwarded_host,
+        client_ip
       }
     });
   }
